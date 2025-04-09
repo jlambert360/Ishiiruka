b4580b633902a2964160c9c895da8872b29da689
Paul Joseph Canavan <ssbmotobug@gmail.com>
2025-04-09 14:24:46 -0400

    P+ dolphin 3.1.0

--- build-appimage.sh
+++ build-appimage.sh
@@ -61,7 +61,7 @@ rm ./AppDir/usr/lib/libglib*
 rm ./AppDir/usr/lib/libgobject*
 
 # Bake an AppImage with the update metadata
-export VERSION="3.0.5"
+export VERSION="3.1.0"
 UPDATE_INFORMATION="${ZSYNC_STRING}" \
 	./Tools/linuxdeploy-update-plugin --appdir=./AppDir/
 
