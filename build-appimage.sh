5a745d41b9c8a2051ebe47e61247b4a4726c1573
Paul Joseph Canavan <ssbmotobug@gmail.com>
2025-05-02 00:09:38 -0400

    3.1.2

--- build-appimage.sh
+++ build-appimage.sh
@@ -61,7 +61,7 @@ rm ./AppDir/usr/lib/libglib*
 rm ./AppDir/usr/lib/libgobject*
 
 # Bake an AppImage with the update metadata
-export VERSION="3.1.1"
+export VERSION="3.1.2"
 UPDATE_INFORMATION="${ZSYNC_STRING}" \
 	./Tools/linuxdeploy-update-plugin --appdir=./AppDir/
 
