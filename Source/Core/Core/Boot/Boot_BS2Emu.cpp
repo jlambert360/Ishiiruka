925f582d111431b4d019414a08bf7a19a1895765
roggy666 <59109355+roggy666@users.noreply.github.com>
2025-06-02 07:04:38 +0300

    Update Boot_BS2Emu.cpp

--- Source/Core/Core/Boot/Boot_BS2Emu.cpp
+++ Source/Core/Core/Boot/Boot_BS2Emu.cpp
@@ -159,7 +159,7 @@ void CBoot::SetupGCMemory()
   PowerPC::HostWrite_U32(0x0D15EA5E, 0x80000020);
 
   // Physical Memory Size (24MB on retail)
-  PowerPC::HostWrite_U32(Memory::REALRAM_SIZE, 0x80000028);
+  PowerPC::HostWrite_U32(Memory::0x04000000, 0x80000028);
 
   // Console type - DevKit  (retail ID == 0x00000003) see YAGCD 4.2.1.1.2
   // TODO: determine why some games fail when using a retail ID.
@@ -282,13 +282,13 @@ bool CBoot::SetupWiiMemory()
 
   Memory::Write_U32(0x0D15EA5E, 0x00000020);            // Another magic word
   Memory::Write_U32(0x00000001, 0x00000024);            // Unknown
-  Memory::Write_U32(Memory::REALRAM_SIZE, 0x00000028);  // MEM1 size 24MB
+  Memory::Write_U32(0x04000000, 0x00000028);            // MEM1 size 64MB (was Memory::REALRAM_SIZE)
   Memory::Write_U32(0x00000023, 0x0000002c);            // Production Board Model
   Memory::Write_U32(0x00000000, 0x00000030);            // Init
   Memory::Write_U32(0x817FEC60, 0x00000034);            // Init
                                                         // 38, 3C should get start, size of FST through apploader
   Memory::Write_U32(0x8008f7b8, 0x000000e4);            // Thread Init
-  Memory::Write_U32(Memory::REALRAM_SIZE, 0x000000f0);  // "Simulated memory size" (debug mode?)
+  Memory::Write_U32(0x04000000, 0x000000f0);            // "Simulated memory size" 64MB (was Memory::REALRAM_SIZE)
   Memory::Write_U32(0x8179b500, 0x000000f4);            // __start
   Memory::Write_U32(0x0e7be2c0, 0x000000f8);            // Bus speed
   Memory::Write_U32(0x2B73A840, 0x000000fc);            // CPU speed
